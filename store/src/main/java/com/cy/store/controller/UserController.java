08b507b0b86c923c0c3bc3f215030b50aecf5941
zjx <zjxcyr@qq.com>
2023-07-13 15:57:29 +0800

    新增异常处理，新增前端页面

---
+++ store/src/main/java/com/cy/store/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.cy.store.controller;
+
+import com.cy.store.entity.User;
+import com.cy.store.service.IUserService;
+import com.cy.store.service.ex.InsertException;
+import com.cy.store.service.ex.UsernameDuplicateException;
+import com.cy.store.util.JsonResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("users")
+public class UserController extends BaseController {
+    @Autowired
+    private IUserService userService;
+
+    @RequestMapping("reg")
+    public JsonResult<Void> reg(User user) {
+        // 调用业务对象执行注册
+        userService.reg(user);
+        // 返回
+        return new JsonResult<Void>(OK);
+    }
+}
+
+
