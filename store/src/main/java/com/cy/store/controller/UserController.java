bd64bdf29029683bdc8423b281b832c839159cd3
zxc <zjxcyr@qq.com>
2023-07-20 14:24:12 +0800

    add 用户-个人资料-controller

--- store/src/main/java/com/cy/store/controller/UserController.java
+++ store/src/main/java/com/cy/store/controller/UserController.java
@@ -7,6 +7,7 @@ import com.cy.store.service.ex.UsernameDuplicateException;
 import com.cy.store.util.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -57,6 +58,48 @@ public class UserController extends BaseController {
         // 调用业务对象执行修改密码
         userService.changePassword(uid, username, oldPassword, newPassword);
         // 返回成功
+        return new JsonResult<Void>(OK);
+    }
+
+    /**
+    @GetMapping("get_by_uid")
+    public JsonResult<User> getByUid(HttpSession session) {
+        // 从HttpSession对象中获取uid
+        // 调用业务对象执行获取数据
+        // 响应成功和数据
+        return null;
+    }
+     */
+
+    @GetMapping("get_by_uid")
+    public JsonResult<User> getByUid(HttpSession session) {
+        // 从HttpSession对象中获取uid
+        Integer uid = getUidFromSession(session);
+        // 调用业务对象执行获取数据
+        User data = userService.getByUid(uid);
+        // 响应成功和数据
+        return new JsonResult<User>(OK, data);
+    }
+
+
+    /**
+     * @RequestMapping("change_info")
+     * public JsonResult<Void> changeInfo(User user, HttpSession session) {
+     *     // 从HttpSession对象中获取uid和username
+     *     // 调用业务对象执行修改用户资料
+     *     // 响应成功
+     *     return null;
+     * }
+     */
+
+    @RequestMapping("change_info")
+    public JsonResult<Void> changeInfo(User user, HttpSession session) {
+        // 从HttpSession对象中获取uid和username
+        Integer uid = getUidFromSession(session);
+        String username = getUsernameFromSession(session);
+        // 调用业务对象执行修改用户资料
+        userService.changeInfo(uid, username, user);
+        // 响应成功
         return new JsonResult<Void>(OK);
     }
 }
