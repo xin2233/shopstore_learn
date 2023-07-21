a100af1f5ec727059f7c1d3569f9d3e8fca4579c
zxc <zjxcyr@qq.com>
2023-07-21 16:18:33 +0800

    add 用户修改-前端页面

--- store/src/main/java/com/cy/store/controller/UserController.java
+++ store/src/main/java/com/cy/store/controller/UserController.java
@@ -136,12 +136,12 @@ public class UserController extends BaseController {
     }
      */
 
-    /** 头像文件大小的上限值(10MB) */
+    /* 头像文件大小的上限值(10MB) */
     public static final int AVATAR_MAX_SIZE = 10 * 1024 * 1024;
-    /** 允许上传的头像的文件类型 */
+    /* 允许上传的头像的文件类型 */
     public static final List<String> AVATAR_TYPES = new ArrayList<String>();
 
-    /** 初始化允许上传的头像的文件类型 */
+    /* 初始化允许上传的头像的文件类型 */
     static {
         AVATAR_TYPES.add("image/jpeg");
         AVATAR_TYPES.add("image/png");
