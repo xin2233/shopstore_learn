2c6dc1b1e00ed8ec1013f31905a6dff1305d2fbd
zxc <zjxcyr@qq.com>
2023-09-05 22:52:02 +0800

    add 新增收货地址-业务层

---
+++ store/src/main/java/com/cy/store/service/ex/AddressCountLimitException.java
@@ -0,0 +1,25 @@
+package com.cy.store.service.ex;
+
+public class AddressCountLimitException extends ServiceException {
+    // Override Methods...
+
+    public AddressCountLimitException() {
+        super();
+    }
+
+    public AddressCountLimitException(String message) {
+        super(message);
+    }
+
+    public AddressCountLimitException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AddressCountLimitException(Throwable cause) {
+        super(cause);
+    }
+
+    protected AddressCountLimitException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ store/src/main/java/com/cy/store/service/impl/AddressServiceImpl.java
@@ -0,0 +1,71 @@
+package com.cy.store.service.impl;
+
+import com.cy.store.entity.Address;
+import com.cy.store.mapper.AddressMapper;
+import com.cy.store.service.IAddressService;
+import com.cy.store.service.ex.AddressCountLimitException;
+import com.cy.store.service.ex.InsertException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class AddressServiceImpl implements IAddressService {
+    @Autowired
+    private AddressMapper addressMapper;
+
+//    @Override
+//    public void addNewAddress(Integer uid, String username, Address address) {
+//        // TODO
+//    }
+//    @Override
+//    public void addNewAddress(Integer uid, String username, Address address) {
+//        // 根据参数uid调用addressMapper的countByUid()方法，统计当前用户的收货地址数据的数量
+//        // 判断数量是否达到上限值
+//        // 是：抛出AddressCountLimitException
+//
+//        // 补全数据：将参数uid封装到参数address中
+//        // 补全数据：根据以上统计的数量，得到正确的isDefault值(是否默认：0-不默认，1-默认)，并封装
+//        // 补全数据：4项日志
+//
+//        // 调用addressMapper的insert()方法插入收货地址数据，并获取返回的受影响行数
+//        // 判断受影响行数是否不为1
+//        // 是：抛出InsertException
+//    }
+
+    @Value("${user.address.max-count}")
+    private int maxCount;
+
+    @Override
+    public void addNewAddress(Integer uid, String username, Address address) {
+        // 根据参数uid调用addressMapper的countByUid(Integer uid)方法，统计当前用户的收货地址数据的数量
+        Integer count = addressMapper.countByUid(uid);
+        // 判断数量是否达到上限值
+        if (count > maxCount) {
+            // 是：抛出AddressCountLimitException
+            throw new AddressCountLimitException("收货地址数量已经达到上限(" + maxCount + ")！");
+        }
+
+        // 补全数据：将参数uid封装到参数address中
+        address.setUid(uid);
+        // 补全数据：根据以上统计的数量，得到正确的isDefault值(是否默认：0-不默认，1-默认)，并封装
+        Integer isDefault = count == 0 ? 1 : 0;
+        address.setIsDefault(isDefault);
+        // 补全数据：4项日志
+        Date now = new Date();
+        address.setCreatedUser(username);
+        address.setCreatedTime(now);
+        address.setModifiedUser(username);
+        address.setModifiedTime(now);
+
+        // 调用addressMapper的insert(Address address)方法插入收货地址数据，并获取返回的受影响行数
+        Integer rows = addressMapper.insert(address);
+        // 判断受影响行数是否不为1
+        if (rows != 1) {
+            // 是：抛出InsertException
+            throw new InsertException("插入收货地址数据时出现未知错误，请联系系统管理员！");
+        }
+    }
+}
