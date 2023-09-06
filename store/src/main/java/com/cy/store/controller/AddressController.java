4f38c169d81116ad32e3a2a6cd36a2e72dd922f7
zxc <zjxcyr@qq.com>
2023-09-06 16:31:04 +0800

    add 新增收货地址-控制器

---
+++ store/src/main/java/com/cy/store/controller/AddressController.java
@@ -0,0 +1,33 @@
+package com.cy.store.controller;
+
+import com.cy.store.entity.Address;
+import com.cy.store.service.IAddressService;
+import com.cy.store.util.JsonResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("addresses")
+public class AddressController extends BaseController {
+    @Autowired
+    private IAddressService addressService;
+
+    @RequestMapping("add_new_address")
+    public JsonResult<Void> addNewAddress(Address address, HttpSession session) {
+        // 从Session中获取uid和username
+        Integer uid = getUidFromSession(session);
+        String username = getUsernameFromSession(session);
+
+        // 调用业务对象的方法执行业务
+        addressService.addNewAddress(uid, username, address);
+        // 响应成功
+        return new JsonResult<Void>(OK);
+    }
+
+    /**
+     * 3.完成后启动项目，打开浏览器先登录，再访问http://localhost:8080/addresses/add_new_address进行测试。
+     */
+}
