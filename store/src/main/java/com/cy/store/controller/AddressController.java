3351de2a7e54b7b35b3a9f41423f340864b32540
zxc <zjxcyr@qq.com>
2023-09-22 11:00:13 +0800

    add 收货地址列表显示-控制器

--- store/src/main/java/com/cy/store/controller/AddressController.java
+++ store/src/main/java/com/cy/store/controller/AddressController.java
@@ -4,10 +4,12 @@ import com.cy.store.entity.Address;
 import com.cy.store.service.IAddressService;
 import com.cy.store.util.JsonResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @RestController
 @RequestMapping("addresses")
@@ -30,4 +32,12 @@ public class AddressController extends BaseController {
     /**
      * 3.完成后启动项目，打开浏览器先登录，再访问http://localhost:8080/addresses/add_new_address进行测试。
      */
+
+    @GetMapping({"", "/"})
+    public JsonResult<List<Address>> getByUid(HttpSession session) {
+        Integer uid = getUidFromSession(session);
+        List<Address> data = addressService.getByUid(uid);
+        return new JsonResult<>(OK, data);
+    }
+
 }
