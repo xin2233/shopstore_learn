3e926649bd9f53e8167959ea2feef09c92ac2cf9
zxc <zjxcyr@qq.com>
2023-09-22 10:54:39 +0800

    add 收货地址列表显示-持久层

--- store/src/main/java/com/cy/store/mapper/AddressMapper.java
+++ store/src/main/java/com/cy/store/mapper/AddressMapper.java
@@ -3,8 +3,12 @@ package com.cy.store.mapper;
 import com.cy.store.entity.Address;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+/**
+ * 处理收货地址数据的持久层接口
+ */
 @Repository
-/** 处理收货地址数据的持久层接口 */
 public interface AddressMapper {
     /**
      * 插入收货地址数据
@@ -21,4 +25,11 @@ public interface AddressMapper {
      * @return 该用户的收货地址数据的数量
      */
     Integer countByUid(Integer uid);
+
+    /**
+     * 查询某用户的收货地址列表数据
+     * @param uid 收货地址归属的用户id
+     * @return 该用户的收货地址列表数据
+     */
+    List<Address> findByUid(Integer uid);
 }
