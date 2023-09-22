ec0cfde133d31adc0de96f8846e66342a5b91034
zxc <zjxcyr@qq.com>
2023-09-22 11:14:51 +0800

    add 默认收货地址-业务层

--- store/src/main/java/com/cy/store/service/impl/AddressServiceImpl.java
+++ store/src/main/java/com/cy/store/service/impl/AddressServiceImpl.java
@@ -4,11 +4,11 @@ import com.cy.store.entity.Address;
 import com.cy.store.mapper.AddressMapper;
 import com.cy.store.service.IAddressService;
 import com.cy.store.service.IDistrictService;
-import com.cy.store.service.ex.AddressCountLimitException;
-import com.cy.store.service.ex.InsertException;
+import com.cy.store.service.ex.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
@@ -98,4 +98,62 @@ public class AddressServiceImpl implements IAddressService {
         return list;
     }
 
+
+//    事务：基于Spring JDBC的事务（Transaction）处理，
+//      使用事务可以保证一系列的增删改操作，
+//      要么全部执行成功，要么全部执行失败。
+//      @Transactional注解可以用来修饰类也可以用来修饰方法。
+//      如果添加在业务类之前，则该业务类中的方法均以事务的机制运行，但是一般并不推荐这样处理。
+//    @Transactional
+//    @Override
+//    public void setDefault(Integer aid, Integer uid, String username) {
+//        // 根据参数aid，调用addressMapper中的findByAid()查询收货地址数据
+//        // 判断查询结果是否为null
+//        // 是：抛出AddressNotFoundException
+//
+//        // 判断查询结果中的uid与参数uid是否不一致(使用equals()判断)
+//        // 是：抛出AccessDeniedException：非法访问
+//
+//        // 调用addressMapepr的updateNonDefaultByUid()将该用户的所有收货地址全部设置为非默认，并获取返回的受影响的行数
+//        // 判断受影响的行数是否小于1(不大于0)
+//        // 是：抛出UpdateException
+//
+//        // 调用addressMapepr的updateDefaultByAid()将指定aid的收货地址设置为默认，并获取返回的受影响的行数
+//        // 判断受影响的行数是否不为1
+//        // 是：抛出UpdateException
+//    }
+
+    @Transactional
+    @Override
+    public void setDefault(Integer aid, Integer uid, String username) {
+        // 根据参数aid，调用addressMapper中的findByAid()查询收货地址数据
+        Address result = addressMapper.findByAid(aid);
+        // 判断查询结果是否为null
+        if (result == null) {
+            // 是：抛出AddressNotFoundException
+            throw new AddressNotFoundException("尝试访问的收货地址数据不存在");
+        }
+
+        // 判断查询结果中的uid与参数uid是否不一致(使用equals()判断)
+        if (!result.getUid().equals(uid)) {
+            // 是：抛出AccessDeniedException
+            throw new AccessDeniedException("非法访问的异常");
+        }
+
+        // 调用addressMapper的updateNonDefaultByUid()将该用户的所有收货地址全部设置为非默认，并获取返回受影响的行数
+        Integer rows = addressMapper.updateNonDefaultByUid(uid);
+        // 判断受影响的行数是否小于1(不大于0)
+        if (rows < 1) {
+            // 是：抛出UpdateException
+            throw new UpdateException("设置默认收货地址时出现未知错误[1]");
+        }
+
+        // 调用addressMapper的updateDefaultByAid()将指定aid的收货地址设置为默认，并获取返回的受影响的行数
+        rows = addressMapper.updateDefaultByAid(aid, username, new Date());
+        // 判断受影响的行数是否不为1
+        if (rows != 1) {
+            // 是：抛出UpdateException
+            throw new UpdateException("设置默认收货地址时出现未知错误[2]");
+        }
+    }
 }
