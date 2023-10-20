d9168e4c2743c2a6c4caec899c6e2c8657eb6153
zxc <zjxcyr@qq.com>
2023-10-20 15:56:50 +0800

    add 商品-热销排行-控制器

--- store/src/main/java/com/cy/store/config/LoginInterceptorConfigurer.java
+++ store/src/main/java/com/cy/store/config/LoginInterceptorConfigurer.java
@@ -35,6 +35,7 @@ public class LoginInterceptorConfigurer implements WebMvcConfigurer {
         patterns.add("/districts/**");
         patterns.add("/products/**");
         patterns.add("/districts/**");
+        patterns.add("/web/index.html");
 
         // 通过注册工具添加拦截器
         registry.addInterceptor(interceptor)
