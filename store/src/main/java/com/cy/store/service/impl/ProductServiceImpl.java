20d45c283d9ae46280b52ac3eb1d3893dd314dab
zxc <zjxcyr@qq.com>
2023-10-20 15:53:48 +0800

    add 商品-热销排行-业务层

---
+++ store/src/main/java/com/cy/store/service/impl/ProductServiceImpl.java
@@ -0,0 +1,29 @@
+package com.cy.store.service.impl;
+
+import com.cy.store.entity.Product;
+import com.cy.store.mapper.ProductMapper;
+import com.cy.store.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/** 处理商品数据的业务层实现类 */
+@Service
+public class ProductServiceImpl implements IProductService {
+    @Autowired
+    private ProductMapper productMapper;
+
+    @Override
+    public List<Product> findHotList() {
+        List<Product> list = productMapper.findHotList();
+        for (Product product : list) {
+            product.setPriority(null);
+            product.setCreatedUser(null);
+            product.setCreatedTime(null);
+            product.setModifiedUser(null);
+            product.setModifiedTime(null);
+        }
+        return list;
+    }
+}
