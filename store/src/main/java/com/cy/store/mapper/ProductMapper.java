ff6b1fd6457d714c365f873732c8f8308f18b252
zxc <zjxcyr@qq.com>
2023-10-20 15:49:55 +0800

    add 商品-热销排行-持久层

---
+++ store/src/main/java/com/cy/store/mapper/ProductMapper.java
@@ -0,0 +1,14 @@
+package com.cy.store.mapper;
+
+import com.cy.store.entity.Product;
+
+import java.util.List;
+
+/** 处理商品数据的持久层接口 */
+public interface ProductMapper {
+    /**
+     * 查询热销商品的前四名
+     * @return 热销商品前四名的集合
+     */
+    List<Product> findHotList();
+}
