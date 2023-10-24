edec2b8b18825cdb3a479f7332a05a69b976a4cb
zxc <zjxcyr@qq.com>
2023-10-24 14:43:10 +0800

    add 购物车-添加购物车-持久层

---
+++ store/src/test/java/com/cy/store/mapper/CartMapperTests.java
@@ -0,0 +1,47 @@
+package com.cy.store.mapper;
+
+import com.cy.store.entity.Cart;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CartMapperTests {
+    @Autowired
+    private CartMapper cartMapper;
+
+    @Test
+    public void insert() {
+        Cart cart = new Cart();
+        cart.setUid(1);
+        cart.setPid(2);
+        cart.setNum(3);
+        cart.setPrice(4L);
+        Integer rows = cartMapper.insert(cart);
+        System.out.println("rows=" + rows);
+    }
+
+    @Test
+    public void updateNumByCid() {
+        Integer cid = 1;
+        Integer num = 10;
+        String modifiedUser = "购物车管理员";
+        Date modifiedTime = new Date();
+        Integer rows = cartMapper.updateNumByCid(cid, num, modifiedUser, modifiedTime);
+        System.out.println("rows=" + rows);
+    }
+
+    @Test
+    public void findByUidAndPid() {
+        Integer uid = 1;
+        Integer pid = 2;
+        Cart result = cartMapper.findByUidAndPid(uid, pid);
+        System.out.println(result);
+    }
+}
+
