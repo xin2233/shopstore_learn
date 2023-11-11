524ad8811ea3f5013d7b22939566d3a81d2722fc
zxc <zjxcyr@qq.com>
2023-11-11 20:12:32 +0800

    add 显示确认订单页-显示勾选的购物车数据-控制器

--- store/src/main/java/com/cy/store/controller/CartController.java
+++ store/src/main/java/com/cy/store/controller/CartController.java
@@ -48,4 +48,14 @@ public class CartController extends BaseController {
         // 返回成功
         return new JsonResult<Integer>(OK, data);
     }
+
+    @GetMapping("list")
+    public JsonResult<List<CartVO>> getVOByCids(Integer[] cids, HttpSession session) {
+        // 从Session中获取uid
+        Integer uid = getUidFromSession(session);
+        // 调用业务对象执行查询数据
+        List<CartVO> data = cartService.getVOByCids(uid, cids);
+        // 返回成功与数据
+        return new JsonResult<>(OK, data);
+    }
 }
